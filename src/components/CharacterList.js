323320d68cb090baa6010745e353801bcc2d8738
fiopwk <flopwiki@gmail.com>
2020-01-24 19:31:13 -0500

    added axios setup

--- src/components/CharacterList.js
+++ src/components/CharacterList.js
@@ -1,10 +1,16 @@
 import React, { useEffect, useState } from "react";
+import axios from 'axios';
 
 export default function CharacterList() {
   // TODO: Add useState to track data from useEffect
+  const [data, setData] = useState([]);
 
   useEffect(() => {
     // TODO: Add API Request here - must run in `useEffect`
+    axios.get('')
+        .then(res => console.log(res.data))
+        .catch(err => console.log(err.message))
+
     //  Important: verify the 2nd `useEffect` parameter: the dependancies array!
   }, []);
 
