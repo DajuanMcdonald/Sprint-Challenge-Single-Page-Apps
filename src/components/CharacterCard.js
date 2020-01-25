fcde3c174e6ef34258ba27753de73e8cac884512
fiopwk <flopwiki@gmail.com>
2020-01-24 19:53:40 -0500

    added styled components to CharacterCard

--- src/components/CharacterCard.js
+++ src/components/CharacterCard.js
@@ -1,5 +1,46 @@
-import React from "react";
+import React from 'react';
+import styled from 'styled-components';
 
-export default function CharacterCard() {
-  return <span>todo: character</span>;
-}
+const CardWrapper = styled.div`
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  padding: 10px;
+  margin: 5%;
+  overflow: hidden;
+`;
+
+const CardImg = styled.img`
+  margin: 2%;
+`;
+
+const CardInfo = styled.div`
+margin: 2%;
+`;
+
+
+
+export default function CharacterCard({character, detail}) {
+  return (
+      <CardWrapper>
+        <CardImg src={character.image} alt={character.name}/>
+        <CardInfo>
+            <span>Name: </span>
+            <h3>{character.name}</h3>
+        </CardInfo>
+          {detail ? (
+              <>
+                 <CardInfo>
+                  <span>Species: </span>
+                  <h3>{character.species}</h3>
+                 </CardInfo>
+                 <CardInfo>
+                  <span>Status: </span>
+                  <h3>{character.status}</h3>
+                 </CardInfo>
+              </>
+          ) : null}
+
+      </CardWrapper>
+  )
+};
