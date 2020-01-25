aa46e1bceec0d33fe75d9a63dd12b71ab9166cac
fiopwk <flopwiki@gmail.com>
2020-01-24 20:08:21 -0500

    imported welcome page

--- src/App.js
+++ src/App.js
@@ -1,14 +1,24 @@
-import React from "react";
+import React from 'react';
+import {Route} from 'react-router-dom';
 import Header from "./components/Header.js";
-import CharacterCard from "./components/CharacterCard";
+import CharacterList from "./components/CharacterList";
+import WelcomePage from "./components/WelcomePage";
 
 
 export default function App() {
-  return (
-    <main>
-      <Header />
-      <CharacterCard/>
-    </main>
+    return (
+        <main>
+            <Header/>
+            <Route exact path='/'>
+               <WelcomePage/>
 
-  );
+            </Route>
+
+            <Route path='/characters'>
+
+                <CharacterList/>
+            </Route>
+        </main>
+
+    );
 }
--- src/components/CharacterList.js
+++ src/components/CharacterList.js
@@ -1,14 +1,21 @@
-import React, { useEffect, useState } from "react";
+import React, { useEffect, useState } from 'react';
 import axios from 'axios';
+import {Link} from 'react-router-dom';
+import CharacterCard from "./CharacterCard";
+
 
 export default function CharacterList() {
   // TODO: Add useState to track data from useEffect
-  const [data, setData] = useState([]);
+  const [characters, setCharacters] = useState([]);
 
   useEffect(() => {
     // TODO: Add API Request here - must run in `useEffect`
-    axios.get('')
-        .then(res => console.log(res.data))
+    axios.get('https://rick-api.herokuapp.com/api/')
+        .then(res => {
+          setCharacters(res.data.results);
+          console.log(res.data.results)
+
+        })
         .catch(err => console.log(err.message))
 
     //  Important: verify the 2nd `useEffect` parameter: the dependancies array!
